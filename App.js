3e055bb04d35fef28dcddf969bdf2a479843ef56
Josh Justice <me@codingitwrong.com>
2022-04-01 07:15:25 -0400

    Add Detox smoke test

--- App.js
+++ App.js
@@ -70,6 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Text testID="hello">Hello, Detox!</Text>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
